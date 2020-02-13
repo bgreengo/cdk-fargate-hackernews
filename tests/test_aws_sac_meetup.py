30e8fc2c98abad20ff97d1faff9bdcba1f34ee58
Blake Green <blake@Blakes-MacBook-Pro.local>
2020-02-12 21:30:02 -0800

    cleanin up

--- tests/test_aws_sac_meetup.py
+++ tests/test_aws_sac_meetup.py
@@ -1,4 +1,5 @@
 import json
+import os
 import pytest
 
 from aws_cdk import core
@@ -6,7 +7,9 @@ from aws_sac_meetup.aws_sac_meetup_stack import AwsSacMeetupStack
 
 def get_template():
     app = core.App()
-    AwsSacMeetupStack(app, "aws-sac-meetup", env=core.Environment(account="987092829714", region="us-west-2"))
+    AwsSacMeetupStack(app, "aws-sac-meetup", env=core.Environment(
+        account=os.environ["CDK_DEFAULT_ACCOUNT"], 
+        region=os.environ["CDK_DEFAULT_REGION"]))
     return json.dumps(app.synth().get_stack("aws-sac-meetup").template)
 
 def test_ecs_service():
